f2154b69a18d4ec59633712c0bd6f3ef2bef68fc
Win Win Htet <WinWinHtet@fusionsol.com>
2025-10-01 10:23:37 +0630

    🐛Fix Bug

--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -21,26 +21,22 @@ const HeaderLayout = () => {
 
   const navigate = useNavigate();
 
-
   const cartCount = useAppSelector(selectCartCount);
-    
+
   const toggleMenu = () => setIsMenuOpen((prev) => !prev);
 
   return (
-    <header className="relative h-16 flex justify-between items-center pl-4 border-b border-[rgba(209,209,216,1)]
-     text-text-primary">
+    <header
+      className="relative h-16 flex justify-between items-center pl-4 border-b border-[rgba(209,209,216,1)]
+     text-text-primary"
+    >
       <Link to="/">
         <img src={cozy} className="w-24 sm:w-auto" alt="logo" />
       </Link>
 
       <nav className="hidden md:flex gap-10 text-sm font-semibold font-secondary">
         {navLinks.map((link) => (
-          <Link
-            key={link.name}
-            to={link.path}
-     
-           
-          >
+          <Link key={link.name} to={link.path}>
             {link.name}
           </Link>
         ))}
@@ -62,23 +58,23 @@ const HeaderLayout = () => {
         </div>
 
         <img src={search} alt="search" className="w-5 h-5" />
-<div className="w-12 sm:w-20 pl-2.5 md:pl-0 border-l border-[rgba(209,209,216,1)] flex items-center justify-center h-16">
-  <div className="relative cursor-pointer"
-  onClick={() => navigate("/")}>
-    <img src={shoppingCard} alt="shoppingCart" className="w-5 h-5" />
-    
-    {cartCount > 0 && (
-      <span
-        className="absolute -top-2 -right-2 text-white text-xs font-semibold w-3.5 h-3.5 flex items-center justify-center rounded-full"
-        style={{ backgroundColor: "rgba(58, 163, 159, 1)" }}
-      >
-        <span className="text-sm text-[8px]">{cartCount}</span>
-      </span>
-    )}
-  </div>
-</div>
+        <div className="w-12 sm:w-20 pl-2.5 md:pl-0 border-l border-[rgba(209,209,216,1)] flex items-center justify-center h-16">
+          <div
+            className="relative cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            <img src={shoppingCard} alt="shoppingCart" className="w-5 h-5" />
 
-
+            {cartCount > 0 && (
+              <span
+                className="absolute -top-2 -right-2 text-white text-xs font-semibold w-3.5 h-3.5 flex items-center justify-center rounded-full"
+                style={{ backgroundColor: "rgba(58, 163, 159, 1)" }}
+              >
+                <span className="text-sm text-[8px]">{cartCount}</span>
+              </span>
+            )}
+          </div>
+        </div>
       </div>
 
       <div className="md:hidden">
@@ -91,8 +87,8 @@ const HeaderLayout = () => {
                   to={link.path}
                   className={({ isActive }) =>
                     isActive
-                      ? "text-teal-600 text-lg font-semibold"
-                      : "text-text-primary text-lg font-semibold hover:text-teal-600"
+                      ? "text-text-primary text-lg font-semibold"
+                      : "text-text-primary text-lg font-semibold"
                   }
                   onClick={() => setIsMenuOpen(false)}
                 >
